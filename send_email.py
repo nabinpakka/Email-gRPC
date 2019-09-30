1058c5268577cb8b585861a181ad0e8955b0451a
hubbleloo <paknans89@gmail.com>
2019-09-30 17:00:56 +0545

    filealso

--- send_email.py
+++ send_email.py
@@ -1,5 +1,8 @@
 import smtplib, ssl
 from email.mime.text import MIMEText
+#for attaching files to the email
+from email.mime.base import MIMEBase
+from email import encoders
 #we can separate subject of msg,from, to etc
 from email.mime.multipart import MIMEMultipart
 
@@ -20,9 +23,21 @@ def send_email(password,msg,receiver,sender):
     message["Subject"]="gRPC Email"
     message["From"] = sender
     message["To"]=receiver
+
+    #attaching file to the mail
+    filename= "tcrt5000.pdf"
+    attachment =open("./tcrt5000.pdf","rb")
+    p = MIMEBase('application','octet-stream')
+    #to change payload into encoded form
+    p.set_payload((attachment).read())
+    #encoding into base64
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
     #writing msg and attaching it to the msg
     body = MIMEText(msg,"plain")
     message.attach(body)
+    message.attach(p)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sender, password)
